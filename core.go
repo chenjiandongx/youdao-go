6457c3d64322d4f779bbecbcc2e001657290adf6
fwindpeak <fwindpeak@gmail.com>
2019-11-15 09:35:28 +0800

    Update: 显示结果增加一点颜色 (#2)
    * 增加更多颜色
    * 使用https

--- core.go
+++ core.go
@@ -12,8 +12,15 @@ import (
 const (
 	KEY     = "1945325576"
 	KEYFROM = "Youdao-dict-v21"
-	URL     = "http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=json&version=1.1&q=%s"
+	URL     = "https://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=json&version=1.1&q=%s"
 	VERSION = "0.1.0"
+)
+const (
+        InfoColor    = "\033[1;34m%s\033[0m"
+        NoticeColor  = "\033[1;36m%s\033[0m"
+        WarningColor = "\033[1;33m%s\033[0m"
+        ErrorColor   = "\033[1;31m%s\033[0m"
+        DebugColor   = "\033[0;36m%s\033[0m"
 )
 
 // 打印版本号
@@ -56,12 +63,15 @@ func Parser(words string) {
 	}
 	// 词意翻译
 	translation, _ := p.M("translation").A(0).String()
+	translation = fmt.Sprintf(ErrorColor, translation)
 	fmt.Printf(">>  %s: %s\n\n", words, translation)
 
 	// 发音
 	usPhonetic, err := p.M("basic").M("us-phonetic").String()
 	if err == nil {
 		ukPhonetic, _ := p.M("basic").M("uk-phonetic").String()
+        usPhonetic = fmt.Sprintf(DebugColor, usPhonetic)
+        ukPhonetic = fmt.Sprintf(DebugColor, ukPhonetic)
 		fmt.Printf("    美:[%s]  英:[%s]\n\n", usPhonetic, ukPhonetic)
 	}
 
@@ -69,6 +79,7 @@ func Parser(words string) {
 	explains, err := p.M("basic").M("explains").Array()
 	if err == nil {
 		for _, value := range explains {
+			value = fmt.Sprintf(WarningColor,value)
 			fmt.Printf("    %s\n", value)
 		}
 		fmt.Println()
@@ -79,7 +90,8 @@ func Parser(words string) {
 	if err == nil {
 		for _, value := range web {
 			item := value.(map[string]interface{})
-			fmt.Printf("    %s%s\n", item["key"], item["value"])
+			v := fmt.Sprintf(InfoColor,item["value"])
+			fmt.Printf("    %s %s\n", item["key"], v)
 		}
 	}
 }
